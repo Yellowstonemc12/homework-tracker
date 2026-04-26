a054bbd36a813e2350eb6fa81a49b551f216375c
Yellowstonemc12 <yellowstonemc12@gmail.com>
2026-04-26 11:27:53 +0800

    app.py

--- app.py
+++ app.py
@@ -58,9 +58,6 @@ with tab1:
             st.session_state.show_filters = not st.session_state.show_filters
 
     if records:
-        levels = sorted(set(r["Level"] for r in records if r["Level"]))
-        subjects = sorted(set(r["Subject"] for r in records if r["Subject"]))
-        homework_list = sorted(set(r["Homework"] for r in records if r["Homework"]))
 
         selected_level = "All"
         selected_subject = "All"
@@ -68,13 +65,13 @@ with tab1:
         student_search = ""
 
     if st.session_state.show_filters:
-            with st.container(border=True):
-                st.write("🔎 Filters")
+        with st.container(border=True):
+            st.write("🔎 Filters")
     
-                selected_level = st.selectbox("Filter by Level", ["All"] + levels)
-                selected_subject = st.selectbox("Filter by Subject", ["All"] + subjects)
-                selected_homework = st.selectbox("Filter by Homework", ["All"] + homework_list)
-                student_search = st.text_input("Search student name")
+            selected_level = st.selectbox("Filter by Level", ["All"] + levels)
+            selected_subject = st.selectbox("Filter by Subject", ["All"] + subjects)
+            selected_homework = st.selectbox("Filter by Homework", ["All"] + homework_list)
+            student_search = st.text_input("Search student name")
 
         filtered = records
 
